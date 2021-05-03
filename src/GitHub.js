e30488d827458921405d4ae34226fd66a1e1e4a1
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-05-03 10:57:14 +0200

    Chap 8

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from 'axios'; 
 import ReactLoading from 'react-loading';
-import { Media, Form, Button } from 'react-bootstrap';
-
-
+import { Media, Form, Button, Nav } from 'react-bootstrap';
 
 class GitHub extends Component {    
     
@@ -41,7 +39,7 @@ class GitHub extends Component {
     render() { 
         const listUsers = this.state.data.map(user =>             
             <Media key={user.id}>
-                <a href={user.html_url}>    
+                <Nav.Link href={`/github/user/${user.login}/${user.id}`}> 
                     <img
                         width={64}
                         height={64}
@@ -49,7 +47,7 @@ class GitHub extends Component {
                         src={user.avatar_url}
                         alt="Generic placeholder"
                     />
-                </a>
+                </Nav.Link>
                 <Media.Body>
                     <h5>Login: {user.login}</h5>
                     <p>Id: { user.id }</p>
